26e0278b68f4dbc347e6352611c1041d6557175f
aqilknz <khairunnz123@gmail.com>
2026-04-27 20:24:29 +0700

    feat: add all minitasks into an interactive menu

--- main.go
+++ main.go
@@ -1,90 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math"
-	"slices"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/aqilknz/koda-b7-go/internals/minitask1"
+	"github.com/aqilknz/koda-b7-go/internals/minitask2"
+	"github.com/aqilknz/koda-b7-go/internals/minitask3"
+	"github.com/aqilknz/koda-b7-go/internals/minitask4"
 )
 
 func main() {
-	// minitask-1
-	radius := 21
-	area, circumference := HitungLingkaran(radius)
-	fmt.Printf("Luas     : %.2f\n", area)
-	fmt.Printf("Keliling : %.2f\n", circumference)
+	scanner := bufio.NewScanner(os.Stdin)
 
-	// minitask-2
-	segitiga(5)
+	for {
+		fmt.Println("===Minitask===")
+		fmt.Println("1. Hitung Luas & Keliling Lingkaran")
+		fmt.Println("2. Segitiga Siku-Siku")
+		fmt.Println("3. Manipulasi Slice")
+		fmt.Println("4. Biodata")
+		fmt.Println("0. Keluar")
+		fmt.Println("")
+		fmt.Print("Pilih Menu: ")
 
-	// minitask-3
-	insertDataToSlice()
+		scanner.Scan()
+		input := strings.TrimSpace(scanner.Text())
+		choose, err := strconv.Atoi(input)
 
-	// minitask-4
-	type Pendidikan struct {
-		nama    string
-		jurusan string
-	}
-
-	type Biodata struct {
-		name      string
-		foto      string
-		email     string
-		umur      int
-		telp      string
-		isMerried bool
-		education []Pendidikan
-	}
-
-	aqil := Biodata{
-		name:      "Ahmad Aqil",
-		foto:      "profile.jpg",
-		email:     "khairunnz123@gmail.com",
-		umur:      21,
-		telp:      "08181812323",
-		isMerried: false,
-		education: []Pendidikan{
-			{
-				nama:    "SMK N 1 Adiwerna",
-				jurusan: "TKJ",
-			},
-			{
-				nama:    "Universitas Siber Asia",
-				jurusan: "Informatika",
-			},
-		},
-	}
-	fmt.Println(aqil)
-
-}
-
-func HitungLingkaran(r int) (float32, float32) {
-	return luasLingkaran(r), kelilingLingkaran(r)
-}
-
-func kelilingLingkaran(r int) float32 {
-	return 2 * math.Pi * float32(r)
-}
-
-func luasLingkaran(r int) float32 {
-	return math.Pi * float32(r*r)
-}
-
-func segitiga(n int) {
-	for i := range n {
-		for range i + 1 {
-			fmt.Print("*")
+		if err != nil {
+			fmt.Println("Error: Masukkan angka yang valid!")
+			continue
 		}
-		fmt.Println()
+
+		switch choose {
+		case 1:
+			fmt.Print("Masukkan jari-jari: ")
+			scanner.Scan()
+			rStr := scanner.Text()
+			r, _ := strconv.Atoi(rStr)
+
+			area, circumference := minitask1.HitungLingkaran(r)
+			fmt.Printf("Hasil => Luas: %.2f dan Keliling: %.2f\n", area, circumference)
+
+		case 2:
+			fmt.Print("Masukkan tinggi segitiga: ")
+			scanner.Scan()
+			tStr := scanner.Text()
+			t, _ := strconv.Atoi(tStr)
+			minitask2.Segitiga(t)
+
+		case 3:
+			minitask3.InsertDataToSlice()
+
+		case 4:
+			minitask4.ShowBiodata()
+		case 0:
+			fmt.Println("Thanks")
+			return
+
+		default:
+			fmt.Println("Pilihan tidak tersedia")
+		}
+
 	}
-}
-func insertDataToSlice() {
-	data := []int{50, 75, 66, 20, 32, 90}
 
-	data = slices.Insert(data, 3, 88)
-
-	fmt.Println(data)
-
-	for i, v := range data {
-		fmt.Printf("Index %d: %d\n", i, v)
-	}
 }
